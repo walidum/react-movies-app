04867f17de138dcd9bc3bbd8cc872072bbdf8e43
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-05-21 12:13:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import Movie from "./pages/movie/movie";
 import AddMovie from "./pages/addmovie/addmovie";
+import P404 from "./pages/404/404";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Switch>
+                <Route exact path="/">
+                    <Redirect to="/movie"/>
+                </Route>
                 <Route path="/movie" component={Movie}/>
                 <Route path="/addmovie" component={AddMovie}/>
+                <Route component={P404}/>
             </Switch>
         </BrowserRouter>
     );
