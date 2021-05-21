06f6c62bf0aeb7fd0c5dd0d71e42ddc71fe38d19
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-05-21 12:09:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Movie from "./pages/movie/movie";
+import AddMovie from "./pages/addmovie/addmovie";
 
 const App = () => {
     return (
-        <>
-            <Movie/>
-        </>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/movie" component={Movie}/>
+                <Route path="/addmovie" component={AddMovie}/>
+            </Switch>
+        </BrowserRouter>
     );
 }
 
---
+++ src/pages/addmovie/addmovie.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+const AddMovie = () => {
+    return (
+        <h1>ADD Movie</h1>
+    )
+}
+export default AddMovie;
