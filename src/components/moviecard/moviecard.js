f6f19e114e97f46878ba98784e27732262198282
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-05-22 10:14:06 +0100

    first commit

--- src/components/moviecard/moviecard.js
+++ src/components/moviecard/moviecard.js
@@ -1,9 +1,13 @@
 import './style.css'
 import {Card} from "@material-ui/core";
 import FilmImg from '../../assets/film.jpg'
+import {useState} from "react";
+import Rating from '@material-ui/lab/Rating';
 
 const MovieCard = (props) => {
     const {movieprops} = props
+    const [value, setValue] = useState(2);
+
     return (
         <Card className={'h-card'}>
             <div className={'img-film'}>
@@ -12,6 +16,15 @@ const MovieCard = (props) => {
             <div className={'info-film'}>
                 <span className={'info-title'}>{movieprops.title} </span>
                 <span className={'info-description'}>{movieprops.description} </span>
+            </div>
+            <div className={'rating'}>
+                <Rating
+                    name="simple-controlled"
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                />
             </div>
         </Card>
     )
