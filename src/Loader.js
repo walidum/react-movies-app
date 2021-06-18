c1c9618267fdcc53924eb0839e32879318ce9af8
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-06-18 15:45:45 +0100

    first commit

---
+++ src/Loader.js
@@ -0,0 +1,22 @@
+import {useState} from "react";
+import AuthContext from "./context/auth.context";
+import App from "./App";
+
+const Loader = () => {
+    const [auth, setAuth] = useState(false)
+    const login = () => {
+        setAuth(true)
+    }
+    const logout = () => {
+        setAuth(false)
+    }
+
+    return (
+        <AuthContext.Provider value={{auth: auth, login: login, logout: logout}}>
+            <App/>
+        </AuthContext.Provider>
+    )
+
+}
+
+export default Loader
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Loader from "./Loader";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Loader/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
